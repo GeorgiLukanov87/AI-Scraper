3cebf30a1ccfeb4350934646a117bc1fc58ed1a7
Georgi St. Lukanov <goshiii87@gmail.com>
2025-04-09 17:43:48 +0300

    update

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from core.process_article import process_article
 if __name__ == "__main__":
     configure_logger()
 
-    url = config_url[0]
+    url = "https://georgilukanov87.pythonanywhere.com/"
     '''    List of urls:
-      0:  "https://en.wikipedia.org/wiki/Talk:Image",
+      0:  "https://en.wikipedia.org/wiki/england",
       1:  "https://www.bbc.com/news/world-us-canada-59703761",
       2:  "https://en.wikipedia.org/wiki/Main_Page",
       3:  "https://www.olx.bg/",
-      4:  "https://georgilukanov87.pythonanywhere.com/",'''
+      4:  "https://georgilukanov87.pythonanywhere.com/",
+      '''
 
     process_article(url, OPENAI_API_KEY, RESULT_FILENAME)
 
