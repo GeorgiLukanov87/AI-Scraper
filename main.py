5c3683a88d2dbd6602b35324722f761210bcc05c
Georgi St. Lukanov <goshiii87@gmail.com>
2025-04-09 18:57:51 +0300

    .

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 # main.py
 
-import logging
-
 from config.config import config_url, OPENAI_API_KEY, RESULT_FILENAME
 from config.logger_setup import configure_logger
 from core.process_article import process_article
 
 if __name__ == "__main__":
     configure_logger()
+    url = config_url[1]
 
-    url = "https://georgilukanov87.pythonanywhere.com/"
-    '''    List of urls:
-      0:  "https://en.wikipedia.org/wiki/england",
-      1:  "https://www.bbc.com/news/world-us-canada-59703761",
-      2:  "https://en.wikipedia.org/wiki/Main_Page",
-      3:  "https://www.olx.bg/",
-      4:  "https://georgilukanov87.pythonanywhere.com/",
-      '''
+    # url = "https://en.wikipedia.org/wiki/bulgaria"
+    # '''    List of urls:
+    #   0:  "https://en.wikipedia.org/wiki/england",
+    #   1:  "https://www.bbc.com/news/world-us-canada-59703761",
+    #   2:  "https://en.wikipedia.org/wiki/Main_Page",
+    #   3:  "https://www.olx.bg/",
+    #   4:  "https://georgilukanov87.pythonanywhere.com/",
+    #   '''
 
     process_article(url, OPENAI_API_KEY, RESULT_FILENAME)
-
